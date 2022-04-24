059b697cc92f82ae48dea834ce0da50088918915
sna2485 <saloni.aggarwal2509@gmail.com>
2022-04-23 20:32:46 -0400

    changes to app.js

--- demo-project/app.js
+++ demo-project/app.js
@@ -1,16 +1,19 @@
+//importing dependencies
 const express = require("express");
 
 const connectDB = require("./config/db");
 var cors = require('cors');
 
+//this is to make the server
 const app = express();
 
 const funcRouter = require("./routes/api/function");
 
+//these are things that express will use to build the middleware
 app.use(express.json())
-app.post('/test', (req, res) => {
-    res.json({requestBody: req.body})  // <==== req.body will be a parsed JSON object
-})
+// app.post('/test', (req, res) => {
+//     res.json({requestBody: req.body})  // <==== req.body will be a parsed JSON object
+// })
 
 // Connect Database
 connectDB();
@@ -21,9 +24,9 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello world yo!"));
+// app.get("/", (req, res) => res.send("Hello world yo!"));
 
-//use Routes
+//this will be the main url that user will go to
 app.use("/api/function", funcRouter);
 
 const port = process.env.PORT || 8082;

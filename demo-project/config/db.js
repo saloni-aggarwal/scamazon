67de8a363f296594f808ab4203561e146dd725a8
sna2485 <saloni.aggarwal2509@gmail.com>
2022-04-24 12:54:24 -0400

    changes to python files

--- demo-project/config/db.js
+++ demo-project/config/db.js
@@ -1,7 +1,9 @@
+//Importing dependencies and url to connect to atlas
 const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+//async method to connect to mongodb atlas that promises a response
 const connectDB = async () => {
   try {
     await mongoose.connect(
